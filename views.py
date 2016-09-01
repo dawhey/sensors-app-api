f4223cb572665777836db9eb9eeee4c6d675ca1a
Dawid Heyman <dawhey@gmail.com>
2016-09-01 14:52:48 +0200

    Added HTML bootstrap layout

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from sensorsapi import app
 from models import *
-from flask import jsonify, request
+from flask import jsonify, request, render_template
 
 
 @app.route('/')
@@ -8,7 +8,7 @@ def index():
     return '<h1>Sensors API</h1> <h2> (currently in development) </h2>'
 
 
-@app.route('/entries', methods=['GET'])
+@app.route('/api/entries', methods=['GET'])
 def get_entries():
     entries = []
     for entry in SensorsEntry.query.all():
@@ -17,15 +17,21 @@ def get_entries():
     return jsonify({'entries': entries})
 
 
-@app.route('/entries/<id>', methods=['GET'])
+@app.route('/api/entries/<id>', methods=['GET'])
 def get_entry(id):
     return jsonify(SensorsEntry.query.get_or_404(id).export_data())
 
 
-@app.route('/entries', methods=['POST'])
+@app.route('/api/entries', methods=['POST'])
 def post_entry():
     entry = SensorsEntry()
     entry.import_data(request.json)
     db.session.add(entry)
     db.session.commit()
     return jsonify(entry.export_data()), 201
+
+
+@app.route('/entries', methods=['GET'])
+def list_entries():
+    entries = SensorsEntry.query.all()
+    return render_template('index.html', entries=entries)
